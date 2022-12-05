f3ec09203ab8e7a9a6cd8e694acf6fbdbfa995a3
Johan <johan.moller040@gmail.com>
2022-12-05 13:09:08 +0100

    Implementing menu functionality

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 let menuButton = document.querySelector("#menu-button");
-let menuItems = document.querySelector("#nav-container");
+let menuContainer = document.querySelector("#nav-container");
 
-console.log(window.innerWidth)
+let homeButton = document.querySelector("#nav-home");
+let homeContainer = document.querySelector("#home-container");
+
+let gameButton = document.querySelector("#nav-game");
+let gameContainer = document.querySelector("#game-container");
+
+let rulesButton = document.querySelector("#nav-rules");
+let rulesContainer = document.querySelector("#rules-container");
+
+let statsButton = document.querySelector("#nav-stats");
+let statsContainer = document.querySelector("#stats-container");
+
 menuButton.addEventListener("click", (event) => {
-    if (window.innerHeight){}
-
-  if (menuItems.style.display === "") {
-    menuItems.style.display = "flex";
+  if (menuContainer.style.display === "") {
+    menuContainer.style.display = "flex";
   } else {
-    menuItems.style.display = "";
+    menuContainer.style.display = "";
   }
 });
 
+homeContainer.style.display = "flex";
 
+let buttons = [homeButton, gameButton, rulesButton, statsButton];
+let containers = [homeContainer, gameContainer, rulesContainer, statsContainer];
+
+console.log(containers);
+
+function displaysContentWhenClicked(button, content) {
+  button.addEventListener("click", () => {
+    for (let i = 0; i < containers.length; ++i) {
+      let element = containers[i];
+
+      if (element === content) {
+        element.style.display = "flex";
+      } else {
+        element.style.display = "";
+      }
+    }
+  });
+}
+
+displaysContentWhenClicked(homeButton, homeContainer);
+displaysContentWhenClicked(gameButton, gameContainer);
+displaysContentWhenClicked(rulesButton, rulesContainer);
+displaysContentWhenClicked(statsButton, statsContainer);
